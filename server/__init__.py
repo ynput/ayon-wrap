bdffdff1b710cad887c0f8d780c28763ebbe8ab5
kalisp <petr.kalis@gmail.com>
2023-10-20 13:59:53 +0200

    Fix host name must be lowercased
    Otherwise project_settings wont find it

--- server/__init__.py
+++ server/__init__.py
@@ -1,5 +1,3 @@
-#from typing import Type
-
 from ayon_server.addons import BaseServerAddon
 
 from .settings import WrapSettings, DEFAULT_WRAP_SETTING
@@ -7,7 +5,7 @@ from .version import __version__
 
 
 class WrpAddon(BaseServerAddon):
-    name = "Wrap"
+    name = "wrap"
     title = "Wrap"
     version = __version__
     settings_model = WrapSettings
