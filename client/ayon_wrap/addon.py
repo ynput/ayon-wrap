b56913c6096783d572dd5ef6c6d375152b575665
kalisp <petr.kalis@gmail.com>
2023-10-24 16:28:47 +0200

    Added location for prehooks

--- client/ayon_wrap/addon.py
+++ client/ayon_wrap/addon.py
@@ -1,7 +1,10 @@
+import os
+
 from openpype.modules import OpenPypeModule, IHostAddon
 from openpype.lib import Logger
 
 log = Logger.get_logger("Wrap")
+WRAP_HOST_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 class WrapAddon(OpenPypeModule, IHostAddon):
@@ -13,3 +16,10 @@ class WrapAddon(OpenPypeModule, IHostAddon):
 
     def get_workfile_extensions(self):
         return [".wrap"]
+
+    def get_launch_hook_paths(self, app):
+        if app.host_name != self.host_name:
+            return []
+        return [
+            os.path.join(WRAP_HOST_DIR, "hooks")
+        ]
