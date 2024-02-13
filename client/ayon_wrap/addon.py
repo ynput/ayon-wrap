9b579d0692e064f367a0453c2dd60c79e626a334
Jakub Trllo <jakub.trllo@gmail.com>
2024-02-13 13:59:44 +0100

    use 'AYONAddon' as base class

--- client/ayon_wrap/addon.py
+++ client/ayon_wrap/addon.py
@@ -1,7 +1,7 @@
 import os
 
-from openpype.modules import OpenPypeModule, IHostAddon, IPluginPaths
 from openpype.lib import Logger
+from openpype.modules import AYONAddon, IHostAddon, IPluginPaths
 
 log = Logger.get_logger("Wrap")
 WRAP_HOST_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -9,7 +9,7 @@ CREATE_PATH = os.path.join(WRAP_HOST_DIR, "plugins",  "create")
 PUBLISH_PATH = os.path.join(WRAP_HOST_DIR, "plugins",  "publish")
 
 
-class WrapAddon(OpenPypeModule, IHostAddon, IPluginPaths):
+class WrapAddon(AYONAddon, IHostAddon, IPluginPaths):
     name = "wrap"
     host_name = "wrap"
 
