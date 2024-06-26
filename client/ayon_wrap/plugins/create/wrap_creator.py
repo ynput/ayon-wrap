7aa4368a9a754a2297649d00a26edf03a6f463c6
Petr Kalis <petr.kalis@gmail.com>
2024-06-26 13:55:14 +0200

    Create multiple instances for single wrap file

--- client/ayon_wrap/plugins/create/wrap_creator.py
+++ client/ayon_wrap/plugins/create/wrap_creator.py
@@ -7,21 +7,27 @@ from ayon_core.lib import (
 )
 from ayon_core.pipeline import (
     CreatedInstance,
+    CreatorError
 )
-from ayon_core.hosts.traypublisher.api.plugin import TrayPublishCreator
+from ayon_traypublisher.api.plugin import TrayPublishCreator
 
 
 class WrapWorkfileCreator(TrayPublishCreator):
+    """"Parses Wrap workfile, looks for all writer nodes names starting `AYON_`
+
+    Expected format of write node name:
+        `AYON_productName_extension` - eg `AYON_modelMain_abc`
+    """
     identifier = "Wrap"
-    product_type = "wrap"
 
     default_variant = "Main"
     extensions = ["wrap"]
 
-    output_node_types = ["SaveGeom"]
+    product_type = None
 
-    def get_instance_attr_defs(self):
-        return []
+    implemented_product_types = [
+        "model", "dataTransform", "dataPD", "render", "image"
+    ]
 
     def create(
         self, product_name: str, instance_data: dict, pre_create_data: dict
@@ -34,28 +40,59 @@ class WrapWorkfileCreator(TrayPublishCreator):
             instance_data = copy.deepcopy(instance_data)
             file_name = file_info["filenames"][0]
             filepath = os.path.join(file_info["directory"], file_name)
-            creator_attributes = {"workfile_path": filepath}
-
+            wrap_instance_data = {"workfile_path": filepath}
             with open(filepath, "r") as fp:
                 content = json.load(fp)
 
-                for node in content["nodes"].values():
-                    if node["nodeType"] not in self.output_node_types:
+                for node_name, node in content["nodes"].items():
+                    if not node_name.startswith("AYON_"):
                         continue
 
-                    creator_attributes["nodeId"] = node["nodeId"]
-                    creator_attributes["output_file_path"] = (node["params"]
-                                                                  ["fileName"]
-                                                                  ["value"])
-                    instance_data["creator_attributes"] = creator_attributes
+                    node_parts = node_name.split("_")
+                    if len(node_parts) != 3:
+                        raise CreatorError(
+                            f"'{node_name} doesn't match to "
+                            "expected write node format "
+                            "'AYON_productName_extension'"
+                        )
+                    product_name = node_parts[1]
+                    product_type = None
+                    for impl_product_type in self.implemented_product_types:
+                        if product_name.startswith(impl_product_type):
+                            product_type = impl_product_type
+                            break
+                    if not product_type:
+                        raise CreatorError(
+                            f"'{node_name} doesn't match to values from "
+                            f"'{self.implemented_product_types}'"
+                        )
+
+                    wrap_instance_data["nodeId"] = node["nodeId"]
+                    wrap_instance_data["nodeName"] = node_name
+                    wrap_instance_data["output_file_path"] = (
+                        node["params"]["fileName"]["value"])
+                    instance_data["wrap"] = wrap_instance_data
 
                     new_instance = CreatedInstance(
-                        self.product_type,
+                        product_type,
                         product_name,
                         instance_data,
                         self
                     )
                     self._store_new_instance(new_instance)
+
+    def get_instance_attr_defs(self):
+        return [
+            FileDef(
+                "filepath",
+                folders=False,
+                single_item=False,
+                extensions=self.extensions,
+                allow_sequences=False,
+                label="Filepath",
+                hidden=True
+            )
+        ]
 
     def get_pre_create_attr_defs(self):
         # Use same attributes as for instance attributes
@@ -73,8 +110,12 @@ class WrapWorkfileCreator(TrayPublishCreator):
     def get_detail_description(self):
         return """# Create instances from Wrap Save nodes
         
-        Looks for node types configured in `self.output_node_types` and for 
-        each of them it created an instance.
+        Looks for writer nodes with names starting by `AYON_`. Tries to match
+        them to final 'product_type' by values in 
+        `self.implemented_product_types`. 
+        
+        Eg. 'AYON_modelMain_abc' name produces instance of 'model' family with
+        'renderMain' product name.
         """
 
     def get_icon(self):
