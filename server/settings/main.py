586ab70d5ec3bda5e1806e4dad2b6cca22bffbdb
Petr Kalis <petr.kalis@gmail.com>
2024-07-15 14:54:19 +0200

    Removed comment from workfile
    <_> pattern is problematic for basic formatting and there is no actual field to pass comment in.

--- server/settings/main.py
+++ server/settings/main.py
@@ -27,7 +27,7 @@ DEFAULT_WRAP_SETTING = {
   "multiple_templates_per_tasks": {
     "workfile_template": {
       "directory_template": "{root[work]}/{project[name]}/{hierarchy}/{folder[name]}/work/{task[name]}/{template_name}",
-      "filename_template": "{project[code]}_{folder[name]}_{task[name]}_{template_name}_{@version}<_{comment}>.{ext}"
+      "filename_template": "{project[code]}_{folder[name]}_{task[name]}_{template_name}_{@version}.{ext}"
     },
     "profiles": []
   }
