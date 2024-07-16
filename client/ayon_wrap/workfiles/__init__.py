8962f37108257beee2e93c168c4084185c2a016b
Petr Kalis <petr.kalis@gmail.com>
2024-07-16 14:44:27 +0200

    Reverted adding WorkfilesToolWindow to init
    This way it should be safer against circular imports

--- client/ayon_wrap/workfiles/__init__.py
+++ client/ayon_wrap/workfiles/__init__.py
@@ -1,10 +1,8 @@
 from .abstract import FileItem
 from .control import WorkfileToolController
-from .widgets.window import WorkfilesToolWindow
 
 
 __all__ = (
     "FileItem",
     "WorkfileToolController",
-    "WorkfilesToolWindow"
 )
