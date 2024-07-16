30cbac5e5204c627b9496cb4e5b553178e08a189
Petr Kalis <petr.kalis@gmail.com>
2024-07-16 12:51:03 +0200

    Add WorkfilesToolWindow to be directly imported

--- client/ayon_wrap/workfiles/__init__.py
+++ client/ayon_wrap/workfiles/__init__.py
@@ -1,8 +1,10 @@
 from .abstract import FileItem
 from .control import WorkfileToolController
+from .widgets.window import WorkfilesToolWindow
 
 
 __all__ = (
     "FileItem",
     "WorkfileToolController",
+    "WorkfilesToolWindow"
 )
