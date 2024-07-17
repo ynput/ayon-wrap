71c5bb2038545314f0b44e73770ea967585beac5
Petr Kalis <petr.kalis@gmail.com>
2024-07-17 12:08:13 +0200

    Fix iterating through items

--- client/ayon_wrap/workfiles/widgets/window.py
+++ client/ayon_wrap/workfiles/widgets/window.py
@@ -175,7 +175,7 @@ class WorkfilesToolWindow(QtWidgets.QDialog):
         template_names = self._controller.get_template_names()
         templates_widget = self._templates_widget
         existing_items = {}
-        for idx in templates_widget.count():
+        for idx in range(templates_widget.count()):
             item = templates_widget.item(idx)
             if not item:
                 break
